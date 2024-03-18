df71799b2efc5f5988557e5b16666df3cbd0ad06
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-03-18 09:30:32 +0200

    Add post contact functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { useEffect, useRef, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import ContactList from "./components/ContactList";
 import { getContacts, saveContact, udpatePhoto } from "./api/ContactService";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   const modalRef = useRef();
   const fileRef = useRef();
   const [data, setData] = useState({});
   const [currentPage, setCurrentPage] = useState(0);
+  const [file, setFile] = useState(undefined);
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    title: "",
+    status: "",
+  });
 
   const getAllContacts = async (page = 0, size = 10) => {
     try {
@@ -19,6 +28,36 @@ function App() {
       console.log(data);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const onChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const handleNewContact = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await saveContact(values);
+      const formData = new FormData();
+      formData.append("file", file, file.name);
+      formData.append("id", data.id);
+      const { data: photoUrl } = await udpatePhoto(formData);
+      toggleModal(false);
+      setFile(undefined);
+      fileRef.current.value = null;
+      setValues({
+        name: "",
+        email: "",
+        phone: "",
+        address: "",
+        title: "",
+        status: "",
+      });
+      getAllContacts();
+    } catch (error) {
+      console.log(error);
+      toastError(error.message);
     }
   };
 
@@ -49,6 +88,7 @@ function App() {
           </Routes>
         </div>
       </main>
+
       {/* Modal */}
       <dialog ref={modalRef} className="modal" id="modal">
         <div className="modal__header">
@@ -57,35 +97,77 @@ function App() {
         </div>
         <div className="divider"></div>
         <div className="modal__body">
-          <form>
+          <form onSubmit={handleNewContact}>
             <div className="user-details">
               <div className="input-box">
                 <span className="details">Name</span>
-                <input type="text" name="name" required />
+                <input
+                  type="text"
+                  value={values.name}
+                  onChange={onChange}
+                  name="name"
+                  required
+                />
               </div>
               <div className="input-box">
                 <span className="details">Email</span>
-                <input type="text" name="email" required />
+                <input
+                  type="text"
+                  value={values.email}
+                  onChange={onChange}
+                  name="email"
+                  required
+                />
               </div>
               <div className="input-box">
                 <span className="details">Title</span>
-                <input type="text" name="title" required />
+                <input
+                  type="text"
+                  value={values.title}
+                  onChange={onChange}
+                  name="title"
+                  required
+                />
               </div>
               <div className="input-box">
                 <span className="details">Phone Number</span>
-                <input type="text" name="phone" required />
+                <input
+                  type="text"
+                  value={values.phone}
+                  onChange={onChange}
+                  name="phone"
+                  required
+                />
               </div>
               <div className="input-box">
                 <span className="details">Address</span>
-                <input type="text" name="address" required />
+                <input
+                  type="text"
+                  value={values.address}
+                  onChange={onChange}
+                  name="address"
+                  required
+                />
               </div>
               <div className="input-box">
                 <span className="details">Account Status</span>
-                <input type="text" name="status" required />
+                <input
+                  type="text"
+                  value={values.status}
+                  onChange={onChange}
+                  name="status"
+                  required
+                />
               </div>
               <div className="file-input">
                 <span className="details">Profile Photo</span>
-                <input type="file" ref={fileRef} name="photo" required />
+                <input
+                  type="file"
+                  onChange={(event) => setFile(event.target.files[0])}
+                  ref={fileRef}
+                  name="photo"
+                  required
+                />
               </div>
             </div>
             <div className="form_footer">
